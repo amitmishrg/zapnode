1e3f1de7f6cc081ca4399a8b3a513f32c59fa5a1
Amit Mishra <amishra@axiamatic.com>
2025-10-15 06:42:29 +0530

    Integrate TRPC and React Query for data fetching; add client and server setup, including API routes and query client configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { Button } from '@/components/ui/button';
-import prisma from '@/lib/db';
+import { Suspense } from 'react';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { Client } from './client';
 
 const Page = async () => {
-  const users = await prisma.user.findMany();
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button variant="secondary">Hello World</Button>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 };
