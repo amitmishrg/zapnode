4c6b6f59237737a2e89dcad0c2d4610d6a223bd0
Amit Mishra <amishra@axiamatic.com>
2025-10-15 05:58:22 +0530

    Add Prisma setup with initial schema, database connection, and user/post models; update dependencies in package.json and .gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@/components/ui/button';
+import prisma from '@/lib/db';
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <Button variant="secondary">Hello World</Button>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   );
 };
