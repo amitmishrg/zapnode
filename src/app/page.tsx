20a30cd51233cc5c6e116c17c1e3349bb7564ba3
Amit Mishra <amishra@axiamatic.com>
2025-10-16 07:19:32 +0530

    Add Inngest integration for workflow management; create new Workflow model in Prisma schema; implement API routes and TRPC mutations for workflow creation, retrieval, updating, and deletion; update package dependencies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
+'use client';
+
 import { requireAuth } from '@/lib/auth-utils';
+import { useTRPC } from '@/trpc/client';
 import { caller } from '@/trpc/server';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const { data: workflows } = useQuery(trpc.getWorkflows.queryOptions());
+  const queryClient = useQueryClient();
 
-  const users = await caller.getUsers();
+  const createWorkflow = useMutation({
+    ...trpc.createWorkflow.mutationOptions(),
+    onSuccess: () => {
+      toast.success('Workflow created');
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <h1>Home</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <div>{JSON.stringify(workflows, null, 2)}</div>
+
+      <Button
+        disabled={createWorkflow.isPending}
+        onClick={() => createWorkflow.mutate({ name: 'Workflow 3' })}
+      >
+        Create Workflow
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,37 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { inngest } from '@/inngest/client';
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
-      },
-    });
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
   }),
+  createWorkflow: protectedProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      await inngest.send({
+        name: 'test/hello.world',
+        data: {
+          email: 'test@test.com',
+        },
+      });
+
+      return { success: true, message: 'Workflow created' };
+    }),
+  updateWorkflow: protectedProcedure
+    .input(z.object({ id: z.string(), name: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return prisma.workflow.update({
+        where: { id: input.id },
+        data: { name: input.name },
+      });
+    }),
+  deleteWorkflow: protectedProcedure
+    .input(z.string())
+    .mutation(({ ctx, input }) => {
+      return prisma.workflow.delete({ where: { id: input } });
+    }),
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
